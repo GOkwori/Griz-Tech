e5e0cd93f7c306dcfa88fc28e08dd533d44fafef
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-05-28 22:02:25 +0100

    Refactor views.py for better readability and maintainability

--- products/views.py
+++ products/views.py
@@ -5,8 +5,8 @@ from django.db.models.functions import Lower
 
 from .models import Product, Category
 
-
 # Create your views here.
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -31,7 +31,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -40,10 +40,12 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didn't enter any search criteria!")
+                messages.error(
+                    request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
@@ -59,13 +61,12 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-
-    """ A view to show individual product details"""
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
     }
-    
-    return render(request, 'products/products_detail.html', context)
+
+    return render(request, 'products/product_detail.html', context)
