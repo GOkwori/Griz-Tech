c78523dbf5e64474b58a20caa0b6a0a8f57b807a
Georgie <george.okwori@outlook.com>
2024-05-31 12:24:00 +0100

    Update ALLOWED_HOSTS in settings.py to include localhost IP address

--- griz_tech/settings.py
+++ griz_tech/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['django-griz-tech.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django-griz-tech.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
