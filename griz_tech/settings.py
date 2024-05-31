c79ca64fb52195a94692f5089c8cdea9174a1ee1
George Okwori <132067023+GOkwori@users.noreply.github.com>
2024-05-31 11:58:26 +0100

    Update SECRET_KEY handling in settings.py

--- griz_tech/settings.py
+++ griz_tech/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xb#f-^b+w0ir-c9^woh8ijp*(@#nx3yxzci84$x9=#@=n1+s=f'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['django-griz-tech.herokuapp.com', 'localhost']
 
@@ -130,7 +130,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 
 # Password validation
